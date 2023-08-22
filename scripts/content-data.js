fb785e52b365d27f54e3cd8a40233980df23e8aa
b4kii <dami4nhalibart@gmail.com>
2023-08-22 17:10:22 +0200

    update: add template for slides

---
+++ scripts/content-data.js
@@ -0,0 +1,20 @@
+export const SLIDES_DATA = [
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sint magni deserunt esse libero harum alias tempore, laudantium consequuntur dolor?",
+        image: "https://images.unsplash.com/photo-1558293842-c0fd3db86157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=722&q=80",
+        alt: "Garden image",
+        firstLinkContent: "Skontaktuj się z nami",
+        secondLinkContent: "Zobacz nasze realizacje",
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sint magni deserunt esse libero harum alias tempore, laudantium consequuntur dolor?",
+        image: "https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Z2FyZGVufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        alt: "Garden image",
+        firstLinkContent: "Skontaktuj się z nami",
+        secondLinkContent: "Zobacz nasze realizacje",
+    },
+];
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,3 +1,21 @@
+import { SLIDES_DATA } from "./content-data";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const slider = document.querySelector("#slider");
+    const template = document.querySelector("#slide-template");
+
+    SLIDES_DATA.forEach((item) => {
+        const slide = template.content.cloneNode(true);
+        slide.querySelector(".slide-title").innerText = item.title;
+        slide.querySelector(".slide-content").innerText = item.content;
+        slide.querySelector(".slide-first-link").querySelector("span").innerText = item.firstLinkContent;
+        slide.querySelector(".slide-second-link").querySelector("span").innerText = item.secondLinkContent;
+        slide.querySelector(".slide-image").setAttribute("alt", item.alt);
+        slide.querySelector(".slide-image").setAttribute("src", item.image);
+        slider.append(slide);
+    })
+});
+
 const button = document.querySelectorAll("[data-slide-button]");
 
 button.forEach((button) => {
@@ -5,18 +23,18 @@ button.forEach((button) => {
         const offset = button.dataset.slideButton === "next" ? 1 : -1;
         const slides = document
             .querySelector("#intro")
-            .querySelector("[data-slider]");
+            .querySelector("#slider");
         const active = slides.querySelector("[data-active]");
 
         let index = [...slides.children].indexOf(active) + offset;
         if (index < 0) index = slides.children.length - 1;
         if (index >= slides.children.length) index = 0;
 
-        slides.style = `transform: translateX(-${index * 100}%)`
+        slides.style = `transform: translateX(-${index * 100}%)`;
 
         slides.children[index].dataset.active = true;
         slides.children[index].setAttribute("aria-hidden", false);
         delete active.dataset.active;
-        active.setAttribute("aria-hidden", true)
+        active.setAttribute("aria-hidden", true);
     });
 });
